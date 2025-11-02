c821dcad7a5146684839b76e06ed798da109d84e
regul <regulunderline@gmail.com>
2025-11-02 12:28:12 +0300

    10.8

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,69 @@
-import Text from './Text';
+import { Text, TextInput, Pressable, View, StyleSheet } from 'react-native';
+import { useFormik } from 'formik';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  flexContainer: {
+    display: 'flex',
+    backgroundColor: 'white',
+  },
+  flexItemA: {
+    flexGrow: 1,
+    color: theme.colors.textSecondary,
+    borderRadius: 10,
+    margin: 5,
+    padding: 20,
+  },
+  flexItemB: {
+    flexGrow: 1,
+    backgroundColor: theme.colors.primary,
+    color: 'white',
+    borderRadius: 10,
+    textAlign: 'center',
+    padding: 20,
+    margin: 5
+  }
+});
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const SignInForm = ({ onSubmit }) => {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  });
+
+  return (
+    <View style={styles.flexContainer}>
+      <TextInput
+        placeholder="Username"
+        value={formik.values.username}
+        onChangeText={formik.handleChange('username')}
+        style={styles.flexItemA}
+      />
+      <TextInput
+        secureTextEntry
+        style={styles.flexItemA}
+        placeholder="Password"
+        value={formik.values.password}
+        onChangeText={formik.handleChange('password')}
+      />
+      <Pressable onPress={formik.handleSubmit}>
+        <Text style={styles.flexItemB}>Sign in</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return <SignInForm onSubmit={onSubmit} />;
 };
 
 export default SignIn;
